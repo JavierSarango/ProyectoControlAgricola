7f75ee3013007d5d6b8b717ca4cc592c78d0ed64
JavierSarango <81376213+JavierSarango@users.noreply.github.com>
2021-12-16 01:02:03 -0500

    mod de paquete modelo - create paquete controlador

---
+++ ProyectoControlAgricola-Home/src/Model/Galpon.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Javier
+ */
+public class Galpon {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ ProyectoControlAgricola-Home/src/Model/Granja.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Home
+ */
+public class Granja {
+    private Long id;
+    private Long c;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+}
